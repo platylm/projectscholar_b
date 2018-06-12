800f9a1e5c02b8825aab1f1a8f59354f3d0429f2
platylm <n_nareenart@kkumail.com>
2018-06-12 23:55:12 +0700

    messages TH-EN

---
+++ messages/th/menu.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 19/10/2560
+ * Time: 21:36
+ */
+
+return [
+    'Home' => 'หน้าหลัก',
+    'Manage Student Info' => 'จัดการข้อมูลนักเรียน',
+    'Candidates List' => 'รายชื่อผู้สมัคร',
+    'Students List' => 'รายชื่อนักเรียนทุน',
+    'Out of scholarships' => 'นักเรียนที่พ้นสภาพทุน',
+    'Public relations' => 'ประชาสัมพันธ์',
+    'Manage News' => 'จัดการข่าวประชาสัมพันธ์',
+    'Appointment Calendar' => 'ปฏิทินนัดหมาย',
+    'Settings' => 'การตั้งค่า',
+    'Manage Scholarships' => 'จัดการทุนการศึกษา',
+    'Manage Acitivity' => 'จัดการกิจกรรม',
+    'Assign Committee' => 'แต่งตั้งกรรมการ',
+    'Funded Grant'=>'รายการเบิกเงินทุนสนับสนุน',
+    'Present Progressive' => 'นำเสนอความก้าวหน้าทุน',
+    'Portfolio-Project' => 'ผลงาน-การแข่งขัน',
+    'Manage Portfolio' => 'จัดการผลงาน',
+    'Manage Project' => 'จัดการโครงงาน-งานวิชาการ',
+    'Manage Activity' => 'จัดการกิจกรรม',
+    'Main Scholarship' => 'ทุนการศึกษาหลัก',
+    'Scholarship each Year' => 'ทุนการศึกษาในแต่ละปี',
+    'Manage Scholarships Detail'=>'จัดการทุนที่เปิด',
+    'Manage Main Scholarships'=>'จัดการทุนหลัก',
+    'Scholarship Advisor' => 'แต่งตั้งอาจารย์ที่ปรึกษาทุน',
+    'Enroll Student'=>'รับนักศึกษาเข้าระบบ',
+    'Public Ralations' => 'ประชาสัมพันธ์',
+    'News'=>'ข่าวสาร',
+    'Calendar'=>'ปฏิทินนัดหมาย',
+    'Scholarship Commitee'=>'แต่งตั้งคณะกรรมการ',
+    'Progress Report' => 'รายงานความก้าวหน้า',
+    'Upload Portfolio' => 'อัปโหลดผลงาน',
+    'Activity' => 'กิจกรรมภาควิชา',
+    'Add News' => 'เพิ่มข่าวสาร',
+    'Setting Teacher' => 'แต่งตั้งอาจารย์',
+    'Upload Project' => 'อัปโหลดโปรเจค',
+    'Upload Activity' => 'อัปโหลดกิจกรรม',
+    'List Student' => 'รายชื่อนักศึกษา',
+    'Back Main' => 'กลับสู่เมนูหลัก',
+    'Approve' => 'คำร้องขออนุมัติ',
+    'Student Progress' => 'รายงานความก้าวหน้า',
+    'Advisor Progress' => 'ที่ปรึกษาโครงงานตรวจความก้าวหน้า',
+    'Committee Progress' => 'กรรมการตรวจความก้าวหน้า',
+    'Staff Progress' => 'ตรวจรายงานความก้าวหน้า',
+    'List Student All' => 'รายชื่อนักศึกษาทั้งหมด',
+    'View Progress' => 'ดูรายงานความก้าวหน้า',
+    'List Name' => 'รายชื่อนักศึกษาทุน'
+
+
+];
+
+
+?>
